893b0806f5d93034cc162fe95c871b47f8ac58d2
jaemin <anjm1020@gmail.com>
2023-04-14 15:50:18 +0900

    Init : 프로젝트 초기화

---
+++ src/main/java/com/konkuk/JaeminServer/JaeminServerApplication.java
@@ -0,0 +1,13 @@
+package com.konkuk.JaeminServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaeminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaeminServerApplication.class, args);
+	}
+
+}
